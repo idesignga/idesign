ed7a35ba5c1a0b5fc935eefa4b1e88938fefc3d2
brianpena <brian@idesignga.com>
2025-06-22 20:15:45 -0400

    Initial commit
    First full site upload.

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "Code Stitch Web Designs",
+    "email": "brian@idesignga.com",
+    "phoneForTel": "706-621-0854",
+    "phoneFormatted": "(706) 621 - 0854",
+    "address": {
+        "lineOne": "First Address Line",
+        "lineTwo": "Second Address Line",
+        "city": "Denver",
+        "state": "CO",
+        "zip": "80206",
+        "country": "US",
+        "mapLink": "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.idesignga.com"
+};
